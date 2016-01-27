0c884c923233bac1e113be6d9c60b2af77b078d5
Zion Ng <zion@intzone.com>
2016-01-28 00:06:59 +0800

    Completed code challenge

---
+++ node-backend-starter-code/README.md
@@ -0,0 +1,5 @@
+![GA Logo](https://raw.github.com/generalassembly/ga-ruby-on-rails-for-devs/master/images/ga.png)
+
+## WDI Instructor Code Challenge
+
+Application deployed at [this link](https://shielded-retreat-39123.herokuapp.com).
--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -1,25 +1,31 @@
+// Modules
 var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/public'))); // serve static files from public
+app.use(bodyParser.urlencoded({ extended: false })); // parse form data
+app.use(bodyParser.json()); // parse JSON
 
-app.use('/', express.static(path.join(__dirname, 'public'));
+// Serve homepage from public folder
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/favorites', function(req, res){
+// Routes
+app.get('/favorites', function (req, res) {
   var data = fs.readFileSync('./data.json');
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
-;
+});
 
-app.get('favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
+app.post('/favorites', function (req, res) {
+  // Request must have "name" and "oid" fields
+  if (!req.body.name || !req.body.oid) {
     res.send("Error");
-    return
-  
+    return;
+  }
+
   var data = JSON.parse(fs.readFileSync('./data.json'));
   data.push(req.body);
   fs.writeFile('./data.json', JSON.stringify(data));
@@ -27,6 +33,8 @@ app.get('favorites', function(req, res){
   res.send(data);
 });
 
-app.list(3000, function(){
-  console.log("Listening on port 3000");
+// Start server and listen to port - get port from environment if available, eg. if deployed on Heroku
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
