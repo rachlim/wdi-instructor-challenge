3a89d4704a22bf863a5cec90e397ea627514937a
Zell Liew <zellwk@gmail.com>
2016-02-05 16:19:35 +0800

    Add SearchMovies spec
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- node-backend-starter-code/test/components/SearchMovies.spec.js
+++ node-backend-starter-code/test/components/SearchMovies.spec.js
@@ -1,18 +1,40 @@
 /* globals it */
-import SearchMovies from '../../js/containers/SearchMovies.jsx'
-import { mount, describeWithDOM } from 'enzyme'
-import { expect } from 'chai'
-import { Provider } from 'react-redux'
-import store from '../store'
 import React from 'react'
+import SearchMovies from '../../js/containers/SearchMovies.jsx'
+import sinon from 'sinon'
+import store from '../store'
+import { expect } from 'chai'
+import { mount, describeWithDOM } from 'enzyme'
+import { Provider } from 'react-redux'
+import { Simulate } from 'react-addons-test-utils'
 
 describeWithDOM('<SearchMovies />', () => {
-  it('should render a form', function () {
+  it('should render a form with one input and one button', function () {
     const wrapper = mount(
       <Provider store={store}>
         <SearchMovies />
       </Provider>
     )
     expect(wrapper.find('form')).to.have.length(1)
+    expect(wrapper.find('input')).to.have.length(1)
+    expect(wrapper.find('button')).to.have.length(1)
   })
+
+  it('should onSubmit when form is submitted', () => {
+    let onSubmit = sinon.spy()
+    const wrapper = mount(
+      <Provider store={store}>
+        <SearchMovies onSubmit={onSubmit} />
+      </Provider>
+    )
+
+    Simulate.submit(wrapper.find('form').node)
+    wrapper.find('form').simulate('submit')
+
+    console.log(onSubmit.calledCount)
+
+    // expect(onSubmit.calledOnce).to.equal(true)
+  })
+
+  it('¯_(ツ)_/¯')
 })
