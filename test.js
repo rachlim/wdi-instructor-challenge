6edf23e111d6317f024f83651256a6b8ce657200
Prima Aulia Gusta <primaulia@gmail.com>
2016-02-07 16:21:24 +0800

    unit test two main path on root and favorites

--- test.js
+++ test.js
@@ -5,14 +5,26 @@ describe('Request to the root path', function() {
   it('Returns a 200 status code', function(done) {
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(error) {
-        if(error) throw error;
-        done();
-      });
+      .expect(200, done);
+  });
+
+  it('Returns a HTML format', function (done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', 'text/html; charset=UTF-8', done);
   });
 });
 
-describe('description', function () {
+describe('Request to the favorite path', function () {
+  it('Returns a 200 status code', function(done) {
+    request(app)
+      .get('/favorites')
+      .expect(200, done);
+  });
 
+  it('Returns a JSON format', function (done) {
+    request(app)
+      .get('/favorites')
+      .expect('Content-Type', 'application/json', done);
+  });
 });
