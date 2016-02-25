34ba74d4c0bd48be8c2fcf0529a732b733f3f784
Prima Aulia Gusta <primaulia@gmail.com>
2016-02-25 18:49:45 +0800

    attempt on implementing trigger on vanilla

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,56 +1,86 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var _DOM = document,
-      _OMDB = function() {
-        var request = new XMLHttpRequest(),
-            _endpoint = '//www.omdbapi.com/?';
+  var _DOM = function() {
+    var _dom = document,
+        event = _dom.createEvent('HTMLEvents');
 
-        return {
-          getMovies: function(params, callback) {
-            request.open('GET', _endpoint + params, true);
-            request.onload = function() {
-              if (request.status >= 200 && request.status < 400) {
-                callback(request.responseText);
-              }
-            };
-            request.send();
+    this.$ = function(el) {
+      // if (!(this instanceof $)) {
+      //   return new $(el);
+      // }
+      // this.el = document.querySelector(el);
+      return _dom.querySelector(el);
+    };
+
+    return {
+      $: this.$,
+      all: function(selector) {
+        return _dom.querySelectorAll(selector);
+      },
+      trigger: function(evt, el) {
+        event.initEvent(evt, true, false);
+        this.$(el).dispatchEvent(event);
+      }
+    };
+  }();
+
+  var _OMDB = function() {
+    var request = new XMLHttpRequest(),
+        _endpoint = '//www.omdbapi.com/?';
+
+    return {
+      getMovies: function(params, callback) {
+        request.open('GET', _endpoint + params, true);
+        request.onload = function() {
+          if (request.status >= 200 && request.status < 400) {
+            callback(request.responseText);
           }
         };
-      }(),
-      _SHARED = function(omdb) {
-        this.resetFlow = function() {
-          if( this.$('button.active') ) this.$('button.active').classList.remove('active');
-          this.$('.result-list').innerHTML = '';
-          // this.$('form').trigger('reset');
-          // this.$(".input input").trigger('blur');
+        request.send();
+      }
+    };
+  }();
 
-          fadeOut($('#search-spinner'));
-        };
+  var _SHARED = function(omdb) {
+    this.resetFlow = function() {
+      this.$('.result-list').innerHTML = '';
+      if( this.$('button.active') ) this.$('button.active').classList.remove('active');
+      _DOM.trigger('reset', 'form');
+      _DOM.trigger('blur', '.input input');
 
-        this.$ = function(el) {
-          // if (!(this instanceof $)) {
-          //   return new $(el);
-          // }
-          // this.el = document.querySelector(el);
-          return _DOM.querySelector(el);
-        };
+      fadeOut(this.$('#search-spinner'));
+    };
 
-        return {
-          $: this.$,
-          all: function(selector) {
-            return _DOM.querySelectorAll(selector);
-          },
-          hide: function(el) {
+    this.listResults = function(results, inFavorite) {
+      console.log('list results');
+    };
 
-          },
-          getMoviesFlow: function(params, type) {
-            omdb.getMovies(params, function(results) {
-              fadeOut(this.$('.alert-container'));
-              resetFlow();
-            });
-          },
+    return {
+      getMoviesFlow: function(params, type) {
+        omdb.getMovies(params, function(results) {
+          console.log(results);
 
-        };
-      }(_OMDB);
+          fadeOut(this.$('.alert-container'));
+          this.resetFlow();
+
+          var results_json = JSON.parse(results);
+
+          if ('True' === results.Response) {
+            this.listResults(results.Search, false);
+            // checkFavStatus();
+            //
+            // //if the trigger for this function comes from "search"
+            // //reset back all the pagination
+            // if(type === 'search') {
+            //   resetPagination(results, params);
+            // }
+          } else {
+            fadeOut(this.$('.paginator'));
+            this.$('.result-list').innerHTML = '<h2>' + results_json.Error + '</h2>';
+          }
+        });
+      }
+    };
+  }(_OMDB);
 
   // FORM Module, all interaction with the form will happen here
   (function($selector) {
@@ -58,12 +88,12 @@ document.addEventListener('DOMContentLoaded', function() {
       e.preventDefault();
 
       // get request parameters from form
-      var form = _SHARED.$('form'),
+      var form = _DOM.$('form'),
           formData = serialize(form),
           formArr = serializeArray(form);
 
       // make sure the css doesn't flash 'active' (yellow)
-      _SHARED.$(".favorite").classList.remove('active');
+      _DOM.$(".favorite").classList.remove('active');
       // console.log(formData, formArr);
 
       if ( ! formArr[0].value ) {
@@ -73,23 +103,23 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
     }, false);
-  })(_SHARED.$('.submit'));
+  })(_DOM.$('.submit'));
 
   // this function gets called to fill the error message placeholder
   function showEmptyError(message) {
-    // _SHARED.$('.paginator').hide();
+    // _DOM.$('.paginator').hide();
 
-    [].forEach.call( _SHARED.all('.alert-container .alert'), function(el) {
+    [].forEach.call( _DOM.all('.alert-container .alert'), function(el) {
       el.textContent = message;
     });
 
-    fadeIn(_SHARED.$('.alert-container'))  ;
+    fadeIn(_DOM.$('.alert-container'))  ;
 
     // resetFlow();
   }
 
   function showSearchResults(formData) {
-    fadeIn(_SHARED.$('#search-spinner'));
+    fadeIn(_DOM.$('#search-spinner'));
     _SHARED.getMoviesFlow(formData, 'search');
   }
 
